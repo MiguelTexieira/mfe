7f3b42d865c800f6d703ba60c35280bac0ea2d11
Miguel Teixeira <miguel@techin.co.uk>
2024-10-17 14:22:30 +0100

    Created container deploy script

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -23,3 +23,16 @@ export default () => {
     </div>
   );
 };
+
+// - uses: shinyinc/action-aws-cli@v1.2
+
+// This updated action will require an AWS_DEFAULT_REGION key, so, for now, we can just add a placeholder.
+
+//       - uses: shinyinc/action-aws-cli@v1.2
+//       - run: aws s3 sync dist s3://${{ secrets.AWS_S3_BUCKET_NAME }}/container/latest
+//         env:
+//           AWS_ACCESS_KEY_ID: ${{ secrets.AWS_ACCESS_KEY_ID }}
+//           AWS_SECRET_ACCESS_KEY: ${{ secrets.AWS_SECRET_ACCESS_KEY }}
+//           AWS_DEFAULT_REGION: ""
+
+// Also, make sure to verify that you are using the correct branch name in your workflow. GitHub now prompts you to name this main by default.
